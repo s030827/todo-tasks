3ccf7d44daeef0ea09410b40209c23303dd72443
Marcus Almeida <s030827@gmail.com>
2018-06-21 14:42:19 -0300

    Add status done to tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -47,7 +47,7 @@ class TasksController < ApplicationController
   end
 
   def task_params
-    params.require(:task).permit(:name, :details, :due_date, :category_id) 
+    params.require(:task).permit(:name, :details, :due_date, :category_id, :done) 
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_21_001140) do
+ActiveRecord::Schema.define(version: 2018_06_21_173141) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2018_06_21_001140) do
     t.bigint "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "done", default: false
     t.index ["category_id"], name: "index_tasks_on_category_id"
   end
 
